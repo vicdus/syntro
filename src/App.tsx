1832ce21038ab3655773225fd8baaff415462d86
Moyang He <iammoyang@gmail.com>
2020-08-17 13:13:07 -0700

    use interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,40 @@
 import React, { useEffect, useState } from "react";
 
-class Employee {
-  status!: "success" | "failed";
-  data!: {
+interface Employee {
+  status: "success" | "failed";
+  data: {
     id: number;
     employee_name: string;
     employee_salary: number;
     employee_age: number;
     profile_image: string;
   };
-  message!: string;
-
-  toString(): string {
-    return this.data.employee_name + " Age " + this.data.employee_age;
-  }
+  message: string;
 }
 
 function App() {
-  const [data, setData] = useState<Employee | null>(null);
+  const [employee, setEmployee] = useState<Employee | null>(null);
 
   useEffect(() => {
     (async () => {
       const response = await fetch(
         "http://dummy.restapiexample.com/api/v1/employee/1"
       );
-      const json = await response.json();
-      const employee = new Employee();
-      Object.assign(employee, json);
-      setData(employee);
+      const employee: Employee = await response.json();
+      setEmployee(employee);
     })();
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        {data ? <p>{data.toString()}</p> : <p>Loading</p>}
+        {employee ? (
+          <p>
+            {employee.data.employee_name + " age " + employee.data.employee_age}
+          </p>
+        ) : (
+          <p>Loading</p>
+        )}
       </header>
     </div>
   );
