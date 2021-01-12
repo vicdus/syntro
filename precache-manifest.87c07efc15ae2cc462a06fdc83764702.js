1adcc57c674f7044ca2f4c416fba6e7c3643820a
Moyang <iammoyang@gmail.com>
2021-01-12 00:27:34 -0800

    Updates

---
+++ precache-manifest.87c07efc15ae2cc462a06fdc83764702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a17826355f301036b78bb4b27e36477",
+    "url": "/syntro/index.html"
+  },
+  {
+    "revision": "6a667fe7a5268bbeab7c",
+    "url": "/syntro/static/js/2.90f97038.chunk.js"
+  },
+  {
+    "revision": "af37ba3010a20e7fe2af6ba6a5e89c7e",
+    "url": "/syntro/static/js/2.90f97038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4a2c2a267602b2bb96",
+    "url": "/syntro/static/js/main.2a668187.chunk.js"
+  },
+  {
+    "revision": "721ddb881e26de0bbefe",
+    "url": "/syntro/static/js/runtime-main.9a454dd0.js"
+  }
+]);
